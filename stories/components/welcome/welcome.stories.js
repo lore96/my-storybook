a864a6717177bba3e7eef4b754a4a61568a7d692
Lorenzo Mussi <lorenzomussi@MBP-di-Lorenzo.homenet.telecomitalia.it>
2018-09-17 20:57:43 +0200

    imported scss and all the stuff

--- stories/components/welcome/welcome.stories.js
+++ stories/components/welcome/welcome.stories.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './welcome.css';
+import styles from './welcome.scss';
 
 import { storiesOf } from '@storybook/react';
 
 storiesOf('Welcome', module)
   .add('Hello', () => (<div>
-    <h1 className="title"> Hello explorer </h1>
+    <h1 className="title"> HELLO EXPLORER 🕵🏻‍♂️</h1>
     <p className="paragraph"> This is where I will collect all my small components, 
     samples, utilities etc. etc. <br />
     I hope you will enjoying seeing my or, however, I hope you will find something usefull to use in to your app!😎 👍 <br />
