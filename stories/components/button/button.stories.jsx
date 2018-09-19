7976515b4aae5972a3aa9230bb2b3d0a0c17b6f6
Lorenzo Mussi <l.mussi96@gmail.com>
2018-09-19 12:08:44 +0200

    my storybook changed

--- stories/components/button/button.stories.jsx
+++ stories/components/button/button.stories.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import MaterialButton from './materialButton';
+import MaterialButton from '../../../src/components/button/materialButton';
 
-import style from './materialButton.scss';
+import style from '../../../src/components/button/materialButton.scss';
 
-storiesOf('Material Button', module)
+storiesOf('Button', module)
   .add('with text', () => (
     <div className="container!">
       <MaterialButton type="button" onClick={()=>{console.log('click')}} textValue="HELLO"></MaterialButton>
