04b07d08af9ab86ec745d3b7b148ae86f65ca456
Lorenzo Mussi <l.mussi96@gmail.com>
2018-09-19 18:22:38 +0200

    have fun

--- stories/Css/circle/circle.stories.jsx
+++ stories/Css/circle/circle.stories.jsx
@@ -7,7 +7,9 @@ import style from '../../../src/pureCssItem/circle/circle.scss';
 storiesOf('CSS', module)
   .add('Circle', () => (
     <div className="atomContainer">
-      <div className="circle rotate__90"></div>
+      <div className="circle rotate__90">
+        <div className="inner-circle"></div>
+      </div>
       <div className="circle rotate__180"></div>  
       <div className="circle rotate__250"></div>
       <div className="nestedCircle"></div>
