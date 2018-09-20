904d80a508dedacc17a5cc2eba08d8fa0b5a1344
Lorenzo Mussi <l.mussi96@gmail.com>
2018-09-20 12:26:02 +0200

    rotating sphere on circle

--- stories/Css/orbitSphere/orbitSphere.stories.jsx
+++ stories/Css/orbitSphere/orbitSphere.stories.jsx
@@ -5,8 +5,20 @@ import { storiesOf } from '@storybook/react';
 import style from './orbitSphereStories.scss';
 
 storiesOf('CSS', module)
-  .add('orbitSphere', () => (
+  .add('Orbit Sphere', () => (
     <div className="orbit-sphere-scene">
         <div className="circle"></div>
+        <div className="orbiting-sphere animating-orbit">
+            <div className="sphere-container">
+                <ul className="sphere">
+                    <li className="plane-v"></li><li className="plane-h"></li>
+                    <li className="plane-v"></li><li className="plane-h"></li>
+                    <li className="plane-v"></li><li className="plane-h"></li>
+                    <li className="plane-v"></li><li className="plane-h"></li>
+                    <li className="plane-v"></li><li className="plane-h"></li>
+                    <li className="plane-v"></li><li className="plane-h"></li>
+                </ul>
+            </div>
+        </div>
     </div>
 ));
