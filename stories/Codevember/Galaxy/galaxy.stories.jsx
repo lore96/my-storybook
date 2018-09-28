af4505df2e795e4706e5dc3b09fd572a0f943ef6
Lorenzo Mussi <l.mussi96@gmail.com>
2018-09-28 16:30:34 +0200

    my galaxy

--- stories/Codevember/Galaxy/galaxy.stories.jsx
+++ stories/Codevember/Galaxy/galaxy.stories.jsx
@@ -4,17 +4,23 @@ import { storiesOf } from '@storybook/react';
 
 import style from '../../../src/codevember/galaxy/galaxy.scss';
 
+const a = [];
+
+for(let i=0; i<500; i++){
+    a.push(i);
+}
+
 storiesOf('Codevember', module)
   .add('Galaxy', () => (
-    <div>
-        <div className="star-container__large">
-            <div className="star"></div>
-        </div>
-        <div className="star-container__medium">
-            <div className="star"></div>
-        </div>
-        <div className="star-container__small">
-            <div className="star"></div>
-        </div>
+    <div className="galaxy-container">
+        {a.map((index) => {
+            return <div className={`star-container-large-${index}`}></div>;
+        })}
+        {a.map((index) => {
+            return <div className={`star-container-medium-${index}`}></div>;
+        })}
+        {a.map((index) => {
+            return <div className={`star-container-small-${index}`}></div>;
+        })}
     </div>
   ));
